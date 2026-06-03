85faf46d2fe018dd0d24c7fdae829344e5965bba
AhmedMohamedomar74 <ahmedmohamedomar74@gmail.com>
2026-06-03 19:29:50 +0300

    init patient serivce

---
+++ patien-service/src/main/java/com/pm/patienservice/PatienServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.patienservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatienServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatienServiceApplication.class, args);
+    }
+
+}
